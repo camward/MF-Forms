8f809062a5aa51a13b99299e040905891cb1ccae
alex <camward@yandex.ru>
2022-06-05 14:44:43 +0400

    передача компонента Loader использовать https://github.com/module-federation/module-federation-examples/tree/master/shared-context

--- projects/side/src/components/Task/Detail/index.tsx
+++ projects/side/src/components/Task/Detail/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StoreWrapper from '../../common/StoreWrapper';
+import Loader from '../../common/Loader';
 import TaskDetail from './detail';
 
 const TaskWrapper = () => {
   return (
     <StoreWrapper hasStore>
+      <Loader />
       <TaskDetail />
     </StoreWrapper>
   );
--- projects/side/src/components/Task/List/index.tsx
+++ projects/side/src/components/Task/List/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StoreWrapper from '../../common/StoreWrapper';
+import Loader from '../../common/Loader';
 import TaskList from './list';
 
 const TaskWrapper = () => {
   return (
     <StoreWrapper hasStore>
+      <Loader />
       <TaskList />
     </StoreWrapper>
   );
